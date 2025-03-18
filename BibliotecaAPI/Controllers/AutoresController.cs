da6f9a618833035a95a01ca769457df76bee1b71
Guillermo Flores <gflores.93@hotmail.com>
2025-03-18 00:30:27 -0600

    feat: add AutoresController with basic CRUD methods

---
+++ BibliotecaAPI/Controllers/AutoresController.cs
@@ -0,0 +1,68 @@
+﻿using BibliotecaAPI.Data;
+using BibliotecaAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BibliotecaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Autor>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return Ok(autor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Autor autor)
+        {
+            if(id != autor.Id)
+            {
+                return BadRequest("Los ids deben de coincidir");
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var registrosBorrados = await context.Autores.Where(x => x.Id == id).ExecuteDeleteAsync();
+            if (registrosBorrados == 0)
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+    }
+}
--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -5,7 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // services
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("name=DefaultConnection"));
+builder.Services.AddControllers();
 
 var app = builder.Build();
+
+// middlewares
+app.MapControllers();
 
 app.Run();
